fdfec9f83a40b2e691df2e93714c05c180fe854d
Chuksexcel <Mekabanz@gmail.com>
2023-03-13 15:09:24 +0000

    add auto lang

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+'''
+creating a localization
+'''
+from flask_babel import Babel, _
+from flask import render_template, Flask, request
+
+
+class Config:
+    '''
+    creating languages
+    'en' 'fr'
+    '''
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+'''
+app configs and calls
+'''
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    determining the best language
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+@app.route('/templates/3-index.html')
+def index() -> str:
+    '''
+    view web page in accordance with locale
+    '''
+    return render_template('3-index.html')
