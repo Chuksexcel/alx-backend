3e73e4d6e988e4a45b24fecfe4df4589e07b8882
Chuksexcel <Mekabanz@gmail.com>
2023-03-13 15:07:06 +0000

    setup a basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+seting up flask
+'''
+
+
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/templates/0-index.html')
+def index():
+    '''
+    linking to 0-index.html
+    '''
+    return render_template('0-index.html')
